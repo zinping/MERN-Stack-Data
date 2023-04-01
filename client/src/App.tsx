8cada95086165887a98b99c22d06e6fb899bbaf4
zinping <haozinping@gmail.com>
2023-04-01 21:50:54 +0200

    refactor: add navbar & flex grid

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,28 @@ import { ThemeProvider } from "@emotion/react";
 import { Box, CssBaseline } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
+import Navbar from "@/pages/navbar";
+import Dashboard from "@/pages/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
-  return <div className="app">
-    <ThemeProvider theme={theme}>
-      <BrowserRouter></BrowserRouter>
-      <CssBaseline />
-      <Box width="100%" height="100%" padding="">
-        
-      </Box>
-    </ThemeProvider>
-  </div>;
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <BrowserRouter></BrowserRouter>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<div>dashboard page</div>} />
+            <Route path="/forecasts" element={<div>Forcasts Page</div>} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/navbar/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PixIcon from "@mui/icons-material/Pix";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+
+  // initial selected page is "dashboard"
+  const [selected, setSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <PixIcon sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          FinForecast
+        </Typography>
+      </FlexBetween>
+
+      {/* RIGHT SIDE with pseudo selector */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            Predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
