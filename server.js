21b0e4c58528e2cb80270791e6192a35b9709e2f
zinping <haozinping@gmail.com>
2023-04-03 21:05:15 +0200

    fix: backend database connection fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
 import Product from "./models/Product.js";
 import Transaction from "./models/Transaction.js";
-import { kpis } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -40,8 +40,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-    await mongoose.connection.db.dropDatabase();
-    KPI.insertMany(kpis);
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
     // Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
