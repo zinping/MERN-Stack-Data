febcbddf434dc15964fea0986ce0f9853363518d
zinping <haozinping@gmail.com>
2023-04-04 21:48:21 +0200

    feat: frontend data display working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,22 +6,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme";
 import Navbar from "@/pages/navbar";
 import Dashboard from "@/pages/dashboard";
+import Forecasts from "@/pages/forecasts";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <BrowserRouter></BrowserRouter>
-        <CssBaseline />
-        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<div>dashboard page</div>} />
-            <Route path="/forecasts" element={<div>Forcasts Page</div>} />
-          </Routes>
-        </Box>
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />}  />
+              <Route path="/forecasts" element={<Forecasts />} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '@/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import "@/index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import { api } from "@/state/api";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
